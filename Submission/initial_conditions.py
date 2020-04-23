aeb6f0d3490887be7d512c10f9ea0fbb72df1359
Lily Whitler <whitler.lily@gmail.com>
2020-04-22 20:02:39 -0700

    wormhole class to make some calculations easier

--- Submission/initial_conditions.py
+++ Submission/initial_conditions.py
@@ -1,32 +1,3 @@
 # Program to set our initial conditions for building our map
+
 import numpy as np
-
-
-def calculate_r(a, rho, ell, M):
-    """
-    Calculate r using Equation (5) from James et al. (2015)
-
-    Parameters
-    ----------
-    a : float
-        Half the height of the wormhole cylinder
-    rho : float
-        Radius of the wormhole cylinder
-    ell : float
-        Proper distance in radial direction
-    M : float
-        Black hole mass
-
-    Returns
-    -------
-    r : float
-        Distance used for calculations as a function of ell
-    """
-    x = 2 * (np.abs(ell) - a) / (np.pi * M)  # Equation 5b
-    # Outside wormhole
-    if np.abs(ell) > a:
-        r = rho + M * (x * np.arctan(x) - 0.5 * np.log(1 + x**2))  # Equation 5a
-    # Inside wormhole
-    else:
-        r = rho  # Equation 5c
-    return r
---
+++ Submission/wormhole.py
@@ -0,0 +1,48 @@
+class Wormhole:
+    def __init__(self, a, rho, W):
+        self.a = a
+        self.rho = rho
+        self.W = W
+        self.M = self.W / 1.4295268191803117  # Footnote 19
+
+
+    def calc_r(self, ell):
+        """
+        Calculate r using Equation (5) from James et al. (2015)
+
+        Parameters
+        ----------
+        ell : float
+            Proper distance in radial direction
+
+        Returns
+        -------
+        r : float
+            Distance used for calculations as a function of ell
+        """
+        x = 2 * (np.abs(ell) - self.a) / (np.pi * self.M)  # 5b
+        # Outside wormhole
+        if np.abs(ell) > self.a:
+            r = (self.rho + self.M * (x * np.arctan(x) -
+                 0.5 * np.log(1 + x**2)))  # 5a
+        # Inside wormhole
+        else:
+            r = self.rho  # 5c
+        return r
+
+
+    def calc_drdl(ell, M):
+        """
+        Calculate dr/dl with the equation given in footnote 19
+
+        Parameters
+        ----------
+        ell : float
+            Proper distance in radial direction
+
+        Returns
+        -------
+        dr/dl : float
+
+        """
+        return 2 / np.pi * np.arctan((2 * ell) / (np.pi * M))
